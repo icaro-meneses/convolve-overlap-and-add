780281253ef12d487e1f97ea9bb4874fad3c7a8e
icaro-meneses <icaro.santana.dev@hotmail.com>
2022-11-30 22:52:48 -0300

    Script de exportação de gráfico em pdf

---
+++ atividade_2.m
@@ -0,0 +1,27 @@
+close all;
+clear all;
+clc;
+
+%%
+% Leitura dos dados de medição
+arquivo_leitura_y_n = fopen('./y_n.txt', 'r');
+
+y_n = fscanf(arquivo_leitura_y_n, "%f\n");
+
+fclose(arquivo_leitura_y_n);
+
+n = [0: 1: length(y_n) - 1 ];
+
+y_fig = figure();
+
+plot(n, y_n, 'r', 'LineWidth', 1);
+hold on;
+xlabel('n', 'fontsize', 12);
+ylabel('y(n)', 'fontsize', 12);
+title({'Resultado da convolução', 'y(n) = x(n) * h(n)'}, 'fontsize', 16);
+grid on;
+hold off;
+
+print (y_fig, "y_n_plot.pdf", "-dpdflatexstandalone");
+system ("pdflatex y_n_plot");
+open y_n_plot.pdf
